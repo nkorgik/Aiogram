66412ebe636ef2b264392e7750fd75837982be16
nkorgik <nkorgik639@gmail.com>
2022-07-26 20:28:58 +0300

    added f

--- intermediate_level/fsm_briefly.py
+++ intermediate_level/fsm_briefly.py
@@ -4,6 +4,7 @@ finite-state-machine
 """
 from aiogram import types, executor, Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
@@ -11,7 +12,8 @@ from config import TOKEN_API
 
 storage = MemoryStorage()
 bot = Bot(TOKEN_API)
-dp = Dispatcher(bot, storage)
+dp = Dispatcher(bot,
+                storage=storage)
 
 
 class ProfileStatesGroup(StatesGroup):
@@ -38,7 +40,46 @@ async def cmd_start(message: types.Message) -> None:
 @dp.message_handler(commands=['create'])
 async def cmd_create(message: types.Message) -> None:
     await message.reply("Let's create your profile! To begin with, send me your photo!")
-    await ProfileStatesGroup.photo.set()
+    await ProfileStatesGroup.photo.set()  # установили состояние фото
+
+
+@dp.message_handler(content_types=['photo'], state=ProfileStatesGroup.photo)
+async def load_photo(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['photo'] = message.photo[0].file_id
+
+    await message.reply('Теперь отправь своё имя!')
+    await ProfileStatesGroup.next()
+
+
+@dp.message_handler(state=ProfileStatesGroup.name)
+async def load_name(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['name'] = message.text
+
+    await message.reply('Сколько тебе лет?')
+    await ProfileStatesGroup.next()
+
+
+@dp.message_handler(state=ProfileStatesGroup.age)
+async def load_age(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['age'] = message.text
+
+    await message.reply('А теперь расскажи немного о себе!')
+    await ProfileStatesGroup.next()
+
+
+@dp.message_handler(state=ProfileStatesGroup.description)
+async def load_desc(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data['description'] = message.text
+        await bot.send_photo(chat_id=message.from_user.id,
+                             photo=data['photo'],
+                             caption=f"{data['name']}, {data['age']}\n{data['description']}")
+
+    await message.reply('Ваша акнета успешно создана!')
+    await state.finish()
 
 
 if __name__ == '__main__':
